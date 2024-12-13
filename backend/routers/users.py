41a4e4182de6a15173952b94ad043d10126e73ae
Rem19Yulia <kyv190591@gmail.com>
2024-12-14 00:44:19 +0300

    Create users.py

---
+++ backend/routers/users.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from backend.db_depends import get_db
+from typing import Annotated
+from models import User
+from schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
+router = APIRouter()
+
+# backend/routers/user.py
+@router.get("/")
+async def all_users(db: Annotated[Session, Depends(get_db)]):
+    users = db.execute(select(User)).scalars().all()
+    return users
+
+@router.get("/{user_id}")
+async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User was not found")
+    return user
+
+@router.post("/create")
+async def create_user(user: CreateUser, db: Annotated[Session, Depends(get_db)]):
+    new_user = User(**user.dict())
+    db.execute(insert(User).values(new_user))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+
+@router.put("/update/{user_id}")
+async def update_user(user_id: int, user: UpdateUser, db: Annotated[Session, Depends(get_db)]):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail="User was not found")
+    
+    db.execute(update(User).where(User.id == user_id).values(**user.dict()))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+
+@router.delete("/delete/{user_id}")
+async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail="User was not found")
+    
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User deleted successfully!'}
