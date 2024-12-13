45bc14aac4d6c058af4b601275a5434c557a7f80
Rem19Yulia <kyv190591@gmail.com>
2024-12-14 00:55:24 +0300

    Update user.py

--- backend/model/user.py
+++ backend/model/user.py
@@ -13,3 +13,15 @@ class User(Base):
     slug = Column(String, unique=True, index=True)
 
     tasks = relationship("Task", back_populates="user")
+    
+@router.delete("/delete/{user_id}")
+async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail="User was not found")
+    
+    # Удаление всех задач, связанных с пользователем
+    db.execute(delete(Task).where(Task.user_id == user_id))
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User and related tasks deleted successfully!'}
